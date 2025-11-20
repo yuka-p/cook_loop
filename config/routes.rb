bfff86b11b59fd34147df83d1ff5d8f63a50c978
yuka-p <yuka.1991.1011@gmail.com>
2025-11-20 11:28:01 +0900

    add: ユーザー登録のフラッシュメッセージを追加

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,12 @@ Rails.application.routes.draw do
   devise_for :users
   get "tops/index"
   root "tops#index"
+  get "home/index"
 
   get "up" => "rails/health#show", as: :rails_health_check
   get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
+
+  resources :my_menus, only: [ :index, :show, :new, :create, :edit, :update, :destroy ]
+  resources :master_menus, only: [ :index, :show, :new, :create, :edit, :update, :destroy ]
 end

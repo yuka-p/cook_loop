e8a9edf68754798cfe5bb3748c4e87087412b15a
yuka-p <yuka.1991.1011@gmail.com>
2025-11-25 13:32:17 +0900

    feat: マイメニュー作成ページ作成

--- app/controllers/my_menus_controller.rb
+++ app/controllers/my_menus_controller.rb
@@ -12,13 +12,27 @@ class MyMenusController < ApplicationController
 
   def new
     @my_menu = MyMenu.new
+    @user_genres = current_user.user_genres.order(:position)
+    @master_menus = MasterMenu.all
+  end
+
+  def create
+    @my_menu = current_user.my_menus.new(my_menu_params)
+
+    if @my_menu.save
+      redirect_to my_menus_path, notice: "メニューを登録しました"
+    else
+      @user_genres = current_user.user_genres.order(:position)
+      @master_menus = MasterMenu.all
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def edit
     @my_menu = MyMenu.find(params[:id])
   end
 
-   def destroy
+  def destroy
     @my_menu.destroy
     redirect_to my_menus_path, notice: "削除しました"
   end
@@ -27,6 +41,10 @@ class MyMenusController < ApplicationController
 
   def set_my_menu
     @my_menu = current_user.my_menus.find(params[:id])
+  end
+
+  def my_menu_params
+    params.require(:my_menu).permit(:title, :genre_id, :master_menu_id)
   end
 
   # create, update, destroy も必要に応じて追加

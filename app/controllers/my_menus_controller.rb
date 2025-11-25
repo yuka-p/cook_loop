df9a98a1e7819e6dfba89aef55eb78c5dbe43075
yuka-p <yuka.1991.1011@gmail.com>
2025-11-25 13:17:35 +0900

    feat: マイメニュー詳細ページ作成

--- app/controllers/my_menus_controller.rb
+++ app/controllers/my_menus_controller.rb
@@ -1,6 +1,9 @@
 class MyMenusController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_my_menu, only: [:show, :destroy]
+
   def index
-    @my_menus = MyMenu.all
+    @my_menus = current_user.my_menus.order(created_at: :desc)
   end
 
   def show
@@ -13,6 +16,17 @@ class MyMenusController < ApplicationController
 
   def edit
     @my_menu = MyMenu.find(params[:id])
+  end
+
+   def destroy
+    @my_menu.destroy
+    redirect_to my_menus_path, notice: "削除しました"
+  end
+
+  private
+
+  def set_my_menu
+    @my_menu = current_user.my_menus.find(params[:id])
   end
 
   # create, update, destroy も必要に応じて追加

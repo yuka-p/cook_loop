f55ce91d485f3f1781f4d41178ff490262a7fbe1
yuka <214949744+yuka-p@users.noreply.github.com>
2025-12-03 14:01:26 +0900

    fix: ジャンル名を日本語表示に修正

--- app/models/master_menu.rb
+++ app/models/master_menu.rb
@@ -4,4 +4,8 @@ class MasterMenu < ApplicationRecord
 
   validates :title, presence: true, length: { maximum: 15 }
   validates :genre, presence: true, inclusion: { in: genres.keys }
+
+  def genre_i18n
+    I18n.t("enums.my_menu.genre.#{genre}")
+  end
 end
